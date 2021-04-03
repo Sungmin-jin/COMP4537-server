a6b0d57c5fe2cc375d7268783af321e39be67b15
Gyeong-Min (Tyler) Lee <tyler7688@gmail.com>
2021-04-03 03:27:20 -0700

    swagger

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const middleware = require('../../middleware/middleware');
-const { check, validationResult } = require('express-validator');
-const connection = require('../../config/db');
-const admin = require('../../admin.json');
+const middleware = require("../../middleware/middleware");
+const { check, validationResult } = require("express-validator");
+const connection = require("../../config/db");
+const admin = require("../../admin.json");
 
 //@route POST api/v1/comments
 //add the comment to the certain post
 //formData: text, postId
 router.post(
-  '/',
-  [middleware, [check('text', 'text is required')]],
+  "/",
+  [middleware, [check("text", "text is required")]],
   (req, res) => {
-    admin.POST['/api/v1/comments']++;
+    admin.POST["/api/v1/comments"]++;
     const { text, postId } = req.body;
     try {
       const sql = `INSERT INTO comment (commentText, postId, userId) VALUES ('${text}', ${postId}, ${req.user.id})`;
@@ -24,15 +24,15 @@ router.post(
         res.json(result);
       });
     } catch (error) {
-      res.status(500).send('server error');
+      res.status(500).send("server error");
     }
   }
 );
 
 //@route GET api/v1/comments/:id
 //@desc get all the comments that belong to the certain posts
-router.get('/:id', middleware, (req, res) => {
-  admin.GET['/api/v1/comments/:id']++;
+router.get("/:id", middleware, (req, res) => {
+  admin.GET["/api/v1/comments/:id"]++;
   try {
     const sql = `SELECT * FROM comment WHERE postId = ${req.params.id}`;
     connection.query(sql, (err, result) => {
@@ -43,17 +43,18 @@ router.get('/:id', middleware, (req, res) => {
       res.json(result);
     });
   } catch (error) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 
 //@route PUT api/v1/comments/:id
 //@desc update a comment by its id and only the owner of the comment can update the comment
-router.put('/:id', middleware, (req, res) => {
-  admin.PUT['/api/v1/comments/:id']++;
+router.put("/", middleware, (req, res) => {
+  admin.PUT["/api/v1/comments/:id"]++;
   try {
-    const { text } = req.body;
-    const sql = `UPDATE comment SET commentText='${text}' WHERE commentId=${req.params.id} AND userId=${req.user.id}`;
+    const { text, id } = req.body;
+    const sql = `UPDATE comment SET commentText='${text}' WHERE commentId=${id} AND userId=${req.user.id}`;
+    console.log(sql);
     connection.query(sql, (err, result) => {
       if (err) {
         console.log(err);
@@ -62,16 +63,17 @@ router.put('/:id', middleware, (req, res) => {
       res.json(result);
     });
   } catch (error) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 
 //@route DELETE api/v1/comments/:id
 //@desc delete the comment by its id and only the ownser of the comment can delete
-router.delete('/:id', middleware, (req, res) => {
-  admin.DELETE['/api/v1/comments/:id']++;
+router.delete("/", middleware, (req, res) => {
+  admin.DELETE["/api/v1/comments/:id"]++;
   try {
-    const sql = `DELETE FROM comment WHERE commentId=${req.params.id} AND userId=${req.user.id}`;
+    const { id } = req.body;
+    const sql = `DELETE FROM comment WHERE commentId=${id} AND userId=${req.user.id}`;
     connection.query(sql, (err, result) => {
       if (err) {
         console.log(err);
@@ -80,7 +82,7 @@ router.delete('/:id', middleware, (req, res) => {
       res.json(result);
     });
   } catch (error) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 

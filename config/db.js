78b96f83ed572928dd5c8c3757464ccde67397df
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2021-04-02 19:37:18 -0700

    changed

--- config/db.js
+++ config/db.js
@@ -2,11 +2,6 @@ const mysql = require('mysql');
 const config = require('config');
 const db = config.get('sqlURI');
 
-const connectDB = mysql.createPool({
-  host: 'us-cdbr-east-03.cleardb.com',
-  user: 'be979b445a77ff',
-  password: '0472db52',
-  database: 'heroku_ad9185d3992d25f',
-});
+const connectDB = mysql.createPool(db);
 
 module.exports = connectDB;

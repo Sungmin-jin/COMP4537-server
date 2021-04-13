c7ccfcf5a5b8848102b7f88a5d10c3801db64adb
Gyeong-Min (Tyler) Lee <tyler7688@gmail.com>
2021-04-13 00:12:10 -0700

    cors

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const connection = require("./config/db");
+const connection = require('./config/db');
 
 // swagger API documentation
-const swaggerUI = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 // cors config to frontEnd url
 const corsOptions = {
-  origin: "https://comp4537-front-end.herokuapp.com",
+  origin: 'https://comp4537-front-end.herokuapp.com',
   credentials: true,
 };
 //body parser
@@ -22,35 +23,35 @@ app.use(cors(corsOptions));
 //default option
 app.use((req, res, next) => {
   res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://comp4537-front-end.herokuapp.com"
+    'Access-Control-Allow-Origin',
+    'https://comp4537-front-end.herokuapp.com'
   );
 
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE"
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
   );
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Content-Type, Accept'
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 //Define routes
-app.use("/api/v1/user", require("./routes/api/user"));
-app.use("/api/v1/auth", require("./routes/api/auth"));
-app.use("/api/v1/posts", require("./routes/api/posts"));
-app.use("/api/v1/comments", require("./routes/api/comments"));
-app.use("/api/v1/admin", require("./routes/api/admin"));
-app.use("/api-docs", swaggerUI.serve);
+app.use('/api/v1/user', require('./routes/api/user'));
+app.use('/api/v1/auth', require('./routes/api/auth'));
+app.use('/api/v1/posts', require('./routes/api/posts'));
+app.use('/api/v1/comments', require('./routes/api/comments'));
+app.use('/api/v1/admin', require('./routes/api/admin'));
+app.use('/api-docs', swaggerUI.serve);
 
-app.get("/api-docs", swaggerUI.setup(swaggerDocument));
-app.get("/deleteAll", (req, res) => {
+app.get('/api-docs', swaggerUI.setup(swaggerDocument));
+app.get('/deleteAll', (req, res) => {
   // let sql = 'DELETE FROM comment';
   // connection.query(sql);
 
-  let sql = "DELETE FROM post";
+  let sql = 'DELETE FROM post';
   connection.query(sql);
 
   // let sql = 'DELETE FROM user';
@@ -58,7 +59,7 @@ app.get("/deleteAll", (req, res) => {
 
   // sql = 'DROP TABLE image';
   // connection.query(sql);
-  res.send("deleted");
+  res.send('deleted');
 });
 
 app.listen(PORT, () => {
